dbaa0a1153bd200bd14a4a43c1abcb0e9254cd2b
Ben Daniels <benjamindaniels21@gmail.com>
2023-10-04 18:42:51 -0400

    create loading spinner

--- script.js
+++ script.js
@@ -3,8 +3,20 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
+// Show loading
+const loading = () => {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+};
+
+// Hide loading
+const complete = () => {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+};
 
 const newQuote = () => {
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
